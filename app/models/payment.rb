15108159d3c0ff6beceb191845f66beeb5819305
Bittu Joju <bjoju@soapboxsample.com>
2021-03-20 12:12:41 -0600

    handle null value

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,14 +5,14 @@ class Payment < ActiveRecord::Base
   before_save :validate_payment, :update_loan
 
   def validate_payment
-    unless amount <= loan.outstanding_balance
+    unless amount <= (loan.outstanding_balance || loan.funded_amount)
       raise "Payment Exceeds Outstanding Balance"
     end
   end
 
   def update_loan
     interest = amount * loan.interest/100
-    outstanding_balance = loan.outstanding_balance || loan.funded_amount
+    outstanding_balance = (loan.outstanding_balance || loan.funded_amount)
     outstanding_balance -= (amount - interest)
     loan.update_attributes({outstanding_balance: outstanding_balance})
   end
